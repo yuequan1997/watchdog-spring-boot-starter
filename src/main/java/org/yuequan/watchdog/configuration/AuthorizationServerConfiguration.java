2b345b5123b8dd48a64d8ab6bae8feb0850741f2
yuequan <yuequan1997@gmail.com>
2018-11-18 15:40:57 +0800

    feat: 配置认证管理器

--- src/main/java/org/yuequan/watchdog/configuration/AuthorizationServerConfiguration.java
+++ src/main/java/org/yuequan/watchdog/configuration/AuthorizationServerConfiguration.java
@@ -3,6 +3,7 @@ package org.yuequan.watchdog.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.yuequan.watchdog.client.Application;
@@ -33,8 +35,9 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Bean
     public TokenStore tokenStore() {
@@ -48,7 +51,12 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore());
+        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.allowFormAuthenticationForClients();
     }
 
     @Bean
