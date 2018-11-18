68aa6c6481543d32cad79261bdeb4b54703cf46e
yuequan <yuequan1997@gmail.com>
2018-11-18 15:40:22 +0800

    feat: 增加资源服务的配置

--- src/main/java/org/yuequan/watchdog/configuration/ResourceServerConfiguration.java
+++ src/main/java/org/yuequan/watchdog/configuration/ResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package org.yuequan.watchdog.configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -20,6 +21,14 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/oauth/*").permitAll();
+        http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+            .and()
+            .requestMatchers().anyRequest()
+            .and()
+            .anonymous()
+            .and()
+            .authorizeRequests()
+            .antMatchers("/oauth/**").permitAll();
     }
 }
