ecd8872ad9675009d7a18248bf9a117e5143df81
yuequan <yuequan1997@gmail.com>
2018-11-19 21:05:19 +0800

    test: 完善测试的schema.sql

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,4 +1,4 @@
-
+DROP TABLE IF EXISTS oauth_client_details;
 create table oauth_client_details (
   client_id VARCHAR(256) PRIMARY KEY,
   name VARCHAR(128),
@@ -14,7 +14,7 @@ create table oauth_client_details (
   additional_information VARCHAR(4096),
   autoapprove VARCHAR(256)
 );
-
+DROP TABLE IF EXISTS oauth_client_token;
 create table oauth_client_token (
   token_id VARCHAR(256),
   token BLOB,
@@ -23,6 +23,7 @@ create table oauth_client_token (
   client_id VARCHAR(256)
 );
 
+DROP TABLE IF EXISTS oauth_access_token;
 create table oauth_access_token (
   token_id VARCHAR(256),
   token BLOB,
@@ -33,16 +34,19 @@ create table oauth_access_token (
   refresh_token VARCHAR(256)
 );
 
+DROP TABLE IF EXISTS oauth_refresh_token;
 create table oauth_refresh_token (
   token_id VARCHAR(256),
   token BLOB,
   authentication BLOB
 );
 
+DROP TABLE IF EXISTS oauth_code;
 create table oauth_code (
   code VARCHAR(256), authentication BLOB
 );
 
+DROP TABLE IF EXISTS oauth_approvals;
 create table oauth_approvals (
 	userId VARCHAR(256),
 	clientId VARCHAR(256),
