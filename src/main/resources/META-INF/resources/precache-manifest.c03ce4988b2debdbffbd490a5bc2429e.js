f65ecd0c11be7a360339de60bd81dbe1b99e01eb
yuequan <yuequan1997@gmail.com>
2018-11-19 15:46:38 +0800

    feat: 添加Watchdog管理界面~~！！

---
+++ src/main/resources/META-INF/resources/precache-manifest.c03ce4988b2debdbffbd490a5bc2429e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f4accb694770290b681425c316da0dc",
+    "url": "/static/media/logo.7f4accb6.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac61c0572e9174eca87c",
+    "url": "/static/js/main.ac61c057.chunk.js"
+  },
+  {
+    "revision": "f3dce14a5382fea5285f",
+    "url": "/static/js/1.f3dce14a.chunk.js"
+  },
+  {
+    "revision": "ac61c0572e9174eca87c",
+    "url": "/static/css/main.c0dbc5b6.chunk.css"
+  },
+  {
+    "revision": "f3dce14a5382fea5285f",
+    "url": "/static/css/1.eb885d32.chunk.css"
+  },
+  {
+    "revision": "6508f56c1a3979940b7eda8db1c28b69",
+    "url": "/index.html"
+  }
+];
